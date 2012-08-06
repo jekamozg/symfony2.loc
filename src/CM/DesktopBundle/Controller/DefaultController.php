68d2ebb3f02e37e205e90bc3edac3cf9102e0d21
jeka <jeka@localhost>
2012-08-06 10:18:19 +0700

    first commit

--- src/CM/DesktopBundle/Controller/DefaultController.php
+++ src/CM/DesktopBundle/Controller/DefaultController.php
@@ -7,47 +7,86 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     
+
      */
-    public function indexAction()
-    {
-        print_r($_POST);
-        die('testing');
+    public function indexAction() {
+        
     }
-    
+
     /**
      * @Template()
      */
-    
     public function loginAction() {
-        $login = 'jeka';
-        $pass = 'jeka+pass_&';
-        
-        
-        $data = array();
-        foreach ($_POST as $key => $value) {
-            $data[$key] = base64_decode($value);
+        $output = array(
+            'id' => 1,
+        );
+        print json_encode($output);
+        die;
+    }
+
+    /**
+     * @Template() 
+     */
+    public function getProjectsAction() {
+        $output = array(
+            'id' => 1,
+        );
+        $projects = array();
+        $project1 = array(
+            1 => array(
+                'id' => 1,
+                'name' => 'Project 1',
+                'link' => 'http://example.com/1',
+                'author' => 1,
+            )
+        );
+        $project2 = array(
+            2 => array(
+                'id' => 2,
+                'name' => 'Project 2',
+                'link' => 'http://example.com/2',
+                'author' => 2,
+            )
+        );
+        $project3 = array(
+            3 => array(
+                'id' => 3,
+                'name' => 'Project 3',
+                'link' => 'http://example.com/3',
+                'author' => 3,
+            )
+        );
+        $projects = $project1 + $project2 + $project3;
+//        
+//        
+////        print_r($this->getRequest());die;
+//        print_r($_POST);die;
+        print json_encode($projects);
+        die;
+    }
+
+    /**
+     * @Template() 
+     */
+    public function getTasksAction() {
+        $data = $_POST;
+        $responce = array();
+        $taskCount = 3;
+        foreach ($data as $key => $value) {
+            for ($index = 0; $index < $taskCount; $index++) {
+                $responce[$value][$index] = array(
+                    'id' => $index,
+                    'name' => 'Task ' . $index,
+                    'link' => 'http://example.com/' . $index,
+                    'author' => $index,
+                );
+            }
         }
-        
-        $output = array();
-        
-        if($login == $data['login'] && $pass == $data['pass']) {
-            $output = array(
-                'success' => true,
-                'message' => 'You already logged in',
-            );
-        }
-        else {
-            $output = array(
-                'success' => false,
-                'message' => 'Login or pass wrong',
-            );
-        }
-        
-        print(json_encode($output));
+        print json_encode($responce);
         die();
     }
+
 }
--- src/CM/UserBundle/Form/Type/LoginFormType.php
+++ src/CM/UserBundle/Form/Type/LoginFormType.php
@@ -3,14 +3,22 @@
 namespace CM\UserBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
-use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+use FOS\UserBundle\Form\Type\UsernameFormType as BaseType;
 
 class LoginFormType extends BaseType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
+        $builder->add('name');
     }
+
+    
+//    public function getDefaultOptions(array $options) {
+//        $options['csrf_protection'] = false;
+//        return $options;
+//    }
+
 
     public function getName()
     {
